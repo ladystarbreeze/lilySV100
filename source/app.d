32190e6b8045dd283e746f8c6b19a65af2c0a9bb
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2020-11-12 10:09:21 +0100

    added file stubs
    Added: app.d, cpu.d

---
+++ source/app.d
@@ -0,0 +1,9 @@
+/* lilySV100 - Casio Loopy emulator written in D. */
+/* Copyright (c) 2020, Lady Starbreeze */
+
+import std.stdio : writeln;
+
+void main()
+{
+    writeln("Hi.");
+}
---
+++ source/cpu/cpu.d
@@ -0,0 +1,4 @@
+/* lilySV100 - Casio Loopy emulator written in D. */
+/* Copyright (c) 2020, Lady Starbreeze */
+
+module cpu;
