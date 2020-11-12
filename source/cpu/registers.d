ff7ef51183be88dbb400f830479f210df8234deb
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2020-11-12 10:47:16 +0100

    added register file and type definitions
    Added: register file (registers.d)
    Added: type definitions (type.d)

---
+++ source/cpu/registers.d
@@ -0,0 +1,59 @@
+/* lilySV100 - Casio Loopy emulator written in D. */
+/* Copyright (c) 2020, Lady Starbreeze */
+
+module registers;
+
+import std.bitmanip : bitfields;
+import type;
+
+alias MACH = 1;
+alias MACL = 0;
+
+/** SuperH status register */
+union Status_Register
+{
+    u32 raw;
+
+    mixin(bitfields!(
+        bool, "t", 1,
+        bool, "s", 1,
+        u32 , "unused1", 2,
+        u32 , "i_mask" , 4,
+        bool, "q", 1,
+        bool, "m", 1,
+        u32 , "unused2", 22));
+}
+
+/** multiply-accumulate register */
+union MA_Register
+{
+    /** raw representation of mach + macl */
+    u64 raw;
+    /** macl/mach */
+    u32[2] mac;
+}
+
+/** SuperH-1 CPU registers */
+struct CPU_Registers
+{
+    /** 16 32-bit general-purpose registers */
+    u32[16] r;
+
+    /* control registers */
+
+    /** status register */
+    Status_Register sr;
+    /** global base register*/
+    u32 gbr;
+    /** vector base register*/
+    u32 vbr;
+
+    /* system registers */
+
+    /** multiply-accumulate high and low registers */
+    MA_Register mac;
+    /** procedure register */
+    u32 pr;
+    /** program counter */
+    u32 pc;
+}
---
+++ source/util/type.d
@@ -0,0 +1,15 @@
+/* lilySV100 - Casio Loopy emulator written in D. */
+/* Copyright (c) 2020, Lady Starbreeze */
+
+module type;
+
+alias u8   = ubyte;
+alias u16  = ushort;
+alias u32  = uint;
+alias u64  = ulong;
+alias umax = size_t;
+
+alias i8  = byte;
+alias i16 = short;
+alias i32 = int;
+alias i64 = long;
